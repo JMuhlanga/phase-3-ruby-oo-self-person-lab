cabeb0336c9fb668a5cdcddd2a84176994ee2b46
Jose Kupeka Muhlanga <jose.muhlanga@student.moringaschool.com>
2022-10-29 01:11:04 +0300

    Completed

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,80 @@
 # your code goes here
+class Person
+    # Class Body
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+
+    #initialization
+    def initialize name
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    #happiness setter method
+    def happiness=(num)
+        @happiness = if num > 10
+            10
+        elsif num < 0
+            0
+        else
+            num
+        end
+    end
+
+    #hygiene setter method
+    def hygiene=(num)
+        @hygiene = if num > 10
+            10
+        elsif num < 0
+            0
+        else
+            num
+        end
+    end
+
+    def get_paid amount
+        self.bank_account += amount
+        #make use of implicit return to return string
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out 
+        self.hygiene -= 3
+        self.happiness += 2
+        "♪ another one bites the dust ♫"
+    end
+
+    def clean?
+        self.hygiene > 7
+    end
+
+    def happy?
+        self.happiness > 7
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            [self, friend].each { |person| person.happiness -= 2 }
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            [self, friend].each { |person| person.happiness += 1 }
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
+end
+
